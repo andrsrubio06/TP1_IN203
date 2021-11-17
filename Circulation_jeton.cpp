56f4563b668303ac83454de137c57b24c948b7d8
Andres <andres-eloy.rubio-carvajal@ensta-paris.fr>
2021-11-17 23:09:36 +0100

    adding jeton

--- Circulation_jeton.cpp
+++ Circulation_jeton.cpp
@@ -1,40 +1,54 @@
 # include <iostream>
 # include <cstdlib>
 # include <mpi.h>
+# include <sstream>
 
-int main( int nargs, char* argv[] )
+int main( int argc, char* argv[] )
 {
-	// On initialise le contexte MPI qui va s'occuper :
-	//    1. Créer un communicateur global, COMM_WORLD qui permet de gérer
-	//       et assurer la cohésion de l'ensemble des processus créés par MPI;
-	//    2. d'attribuer à chaque processus un identifiant ( entier ) unique pour
-	//       le communicateur COMM_WORLD
-	//    3. etc...
 
-	MPI_Init( &nargs, &argv );
-	// Pour des raison préfère toujours cloner le communicateur global
-	// MPI_COMM_WORLD qui gère l'ensemble des processus lancés par MPI.
+    int initValue = (argc > 1) ? atoi(argv[1]) : 10;
+
+	MPI_Init( &argc, &argv );
+	
+	int rank, numTasks, next, prev;
+
 	MPI_Comm globComm;
 	MPI_Comm_dup(MPI_COMM_WORLD, &globComm);
-	// On interroge le communicateur global pour connaître le nombre de processus
-	// qui ont été lancés par l'utilisateur :
-	int nbp;
-	MPI_Comm_size(globComm, &nbp);
-	// On interroge le communicateur global pour connaître l'identifiant qui
-	// m'a été attribué ( en tant que processus ). Cet identifiant est compris
-	// entre 0 et nbp-1 ( nbp étant le nombre de processus qui ont été lancés par
-	// l'utilisateur )
-	int rank;
+	MPI_Comm_size(globComm, &numTasks);
 	MPI_Comm_rank(globComm, &rank);
 
-	// On peut maintenant commencer à écrire notre programme parallèle en utilisant les
-	// services offerts par MPI.
-	std::cout << "Hello World, I'm processus " << rank << " on " << nbp << " processes.\n";
+	prev = rank - 1;
+    next = rank + 1;
+    if (rank == 0) prev = numTasks - 1;
+    if (rank == (numTasks - 1)) next = 0;
 
-	// A la fin du programme, on doit synchroniser une dernière fois tous les processus
-	// afin qu'aucun processus ne se termine pendant que d'autres processus continue à
-	// tourner. Si on oublie cet instruction, on aura une plantage assuré des processus
-	// qui ne seront pas encore terminés.
+	MPI_Status status;
+
+	int jeton;
+
+    if (rank == 0) {
+
+        jeton = initValue;
+        MPI_Send(&jeton, 1, MPI_INT, next, 0, MPI_COMM_WORLD);
+        MPI_Recv(&jeton, 1, MPI_INT, prev, 0, MPI_COMM_WORLD, &status);
+    
+	} else {
+        MPI_Recv(&jeton, 1, MPI_INT, prev, 0, MPI_COMM_WORLD, &status);
+        MPI_Send(&(++jeton), 1, MPI_INT, next, 0, MPI_COMM_WORLD);
+    }
+
+    if (rank == 0) {
+        std::cout 
+			<< std::endl
+            << "The final value of the jeton: " 
+            << jeton 
+            << ", initial value of the jeton:"
+            << initValue
+            << std::endl;
+    }
+
+
+
 	MPI_Finalize();
 	return EXIT_SUCCESS;
 }
